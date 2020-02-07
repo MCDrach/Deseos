1b9799a5a554badab2fd4805297b584d3d3ad04d
MCDRACH <darwinchilon@gmail.com>
2020-02-06 22:36:37 -0500

    se creo lista

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +7,12 @@ import { Injectable } from '@angular/core';
 export class DeseosService {
 
   listas: any [] = [];
-  constructor() { }
+
+  constructor() { 
+    const lista1 = new Lista('recolectar piedras del infinito');
+    const lista2 = new Lista('Heroes a desaparecer');
+    this.listas.push(lista1,lista2);
+    console.log(this.listas);
+    
+  }
 }
